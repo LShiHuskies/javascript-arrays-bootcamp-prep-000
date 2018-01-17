dd36bd13090c7fbd9b54c977be1255b9b172f520
Louis Shi <louis.shi@uconn.edu>
2018-01-17 03:54:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers",
                      "hundred grand",
                      "kitkat",
                      "skittles"]
-function addElementToBeginningOfArray (array, element) {
+function addElementToBeginningOfArray ([1], 'foo') {
 cities.unshift("Philadelphia")
 return moreCities
 }
