b3566169a9123f600ef668aabd0ce01d80aaf6b6
Louis Shi <louis.shi@uconn.edu>
2018-01-17 04:47:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,8 @@ return newarray
                        return array.slice(1)
                      }
                      function destructivelyRemoveElementFromEndOfArray(array) {
-                       return array.pop()
+                       array.pop()
+                       return array
                      }
                      function removeElementFromEndOfArray (array) {
                        var iceCreams = ["chocolate", "vanilla", "raspberry"]
