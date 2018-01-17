31508b55b60769dc688d126574ab2551a0e99380
Louis Shi <louis.shi@uconn.edu>
2018-01-17 04:44:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ return newarray
                        return array
                      }
                      function removeElementFromBeginningOfArray (array) {
-                       array.slice(0)
+                       array.slice(1)
                        return array
                      }
                      function destructivelyRemoveElementFromEndOfArray(array) {
