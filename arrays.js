22b2ed1889046a0e1e2f14ce711fc49ed5e02341
Louis Shi <louis.shi@uconn.edu>
2018-01-17 03:06:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ["snickers",
                      "kitkat",
                      "skittles"]
 function addElementToBeginningOfArray (array, element) {
-oonst var cities = ["New York", "San Francisco"]
+const var cities = ["New York", "San Francisco"]
 return cities = ["Philadelphia", ...cities]
 }
                      function addElementToEndOfArray (array, element) {
