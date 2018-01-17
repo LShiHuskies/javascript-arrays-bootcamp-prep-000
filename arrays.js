6bd06ab5893676b38a066df12e7eda0e2e5739cd
Louis Shi <louis.shi@uconn.edu>
2018-01-17 04:11:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers",
                      "hundred grand",
                      "kitkat",
                      "skittles"]
-function addElementToBeginningOfArray ([1], 'foo') {
+function addElementToBeginningOfArray (array, element) {
 return 'foo' + [1]
 }
                      function addElementToEndOfArray (array, element) {
